1aa96bb75204b6690346d3a7730f0e5289a7f011
Ben alaluf <94129183+benalaluf@users.noreply.github.com>
2023-10-10 21:39:47 +0300

    d

--- main_client.py
+++ main_client.py
@@ -3,4 +3,4 @@ import os
 from src.app.client_app import ClientApp
 
 if __name__ == '__main__':
-    client = ClientApp('127.0.0.1', 6968, f'/Users/blu/Desktop').main()
+    client = ClientApp('127.0.0.1', 6967, f'/Users/blu/Desktop').main()
--- main_server.py
+++ main_server.py
@@ -1,7 +1,6 @@
 import os
 
 from src.app.server_app import ServerApp
-from src.connections.server import Server
 
 if __name__ == '__main__':
-    server = ServerApp('127.0.0.1', 6968, f'/Users/blu/Desktop').main()
+    server = ServerApp('127.0.0.1', 6967, f'/Users/blu/Desktop').main()
